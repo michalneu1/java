99ad2ad0ad41a50aef87dcfdb5ee0c6b33904ff2
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2026-05-28 19:16:17 +0200

    refactor

--- hello-world/src/main/java/org/example/Main.java
+++ hello-world/src/main/java/org/example/Main.java
@@ -1,5 +1,4 @@
 package org.example;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,7 +6,5 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("hello world");
-
-
     }
 }
