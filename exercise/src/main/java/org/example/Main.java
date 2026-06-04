6101826f8f84285e1704a763e1212472eeefc9ea
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2026-06-04 12:14:11 +0200

    refactor: empty line

--- exercise/src/main/java/org/example/Main.java
+++ exercise/src/main/java/org/example/Main.java
@@ -38,7 +38,6 @@ public class Main {
         System.out.println("Podaj wysokosc piramidy");
         int a = scanner.nextInt();
         for (int i = 1; i <= a; i++) {
-
             for (int j = 1; j <= a - i; j++) {
                 System.out.print(' ');
             }
