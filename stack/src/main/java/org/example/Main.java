49b3a0dce1095e195b28b35e4bc69d2a2034a340
MichalNeuman <146724475+MichalNeuman@users.noreply.github.com>
2026-06-08 00:37:13 +0200

    test: main

---
+++ stack/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(3);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        stack.remove(2);
+
+        System.out.println("Size: " + stack.getSize());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
